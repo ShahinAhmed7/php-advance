7b3a522ca359b0491c942b5cbd0f3e533617e51b
Shahin <meshahin007@gmail.com>
2018-11-15 01:06:23 +0600

    variable1

---
+++ variable1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+<form method="GET" action="">
+	<input type="text" name="person">
+	<button>SUBMIT</button>
+</form>
+
+
+<?php  
+	
+	$name = $_GET['person'];
+
+	echo $name.' How are you?';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
