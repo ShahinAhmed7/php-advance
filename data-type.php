a46c787848796680031f19f4264b6df711be0b1a
Shahin <meshahin007@gmail.com>
2018-11-15 23:55:40 +0600

    Data Type

---
+++ data-type.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+<?php  
+	
+
+	$name = 'Coding is fun';
+
+	//Integer
+	$name = 20;
+
+
+	//Float
+	$name = 20.784;
+
+
+	//Boolean
+	//true = 1;
+	//false = 0;
+
+	//Array
+	$names = array('Shahin', 'Shahin2', 'Shahin3');
+
+	echo $names['0'];
+
+
+
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
