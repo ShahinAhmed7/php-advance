1c6af67444b7e9dda5f65b63282094ff3f504048
Shahin <meshahin007@gmail.com>
2018-11-15 11:18:04 +0600

    str-properties

---
+++ strlen.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+<?php  
+	
+
+
+	//echo strlen('Hello Shahin');
+
+	//echo str_word_count('Hello Shahin');
+
+	//echo strrev('nihahS olleH');
+
+	//echo strpos('Hi shahin', 'shahin');
+
+	echo str_replace('Example', 'Shahin', 'Example Hello');
+
+
+
+
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
