eff44db0019df7eaa1cf6ae2b1158d9b0b98c33e
Shahin <meshahin007@gmail.com>
2018-11-16 22:02:24 +0600

    commit

---
+++ comparicon.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+
+<?php  
+	
+	
+	$buy = 10;
+
+	$cost = 10;
+
+	if($buy >= $cost){
+		echo 'true';
+	}else{
+		echo 'false';
+	}
+
+
+
+
+
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
