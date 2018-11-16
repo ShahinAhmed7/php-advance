da0b98f462b21df5632c030c72a590103a5875a0
Shahin <meshahin007@gmail.com>
2018-11-17 00:09:32 +0600

    commit

---
+++ increase-decrease.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+
+<?php  
+	
+	
+	number 
+
+
+
+
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
