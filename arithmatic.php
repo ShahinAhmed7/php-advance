1ed4098d699e411975b2f6856bceb6e09746cd17
Shahin <meshahin007@gmail.com>
2018-11-16 21:47:00 +0600

    commit

---
+++ arithmatic.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+<?php  
+	
+
+	echo 5**5;
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
