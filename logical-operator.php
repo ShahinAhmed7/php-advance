4ba114b85dd85f6c79a36dcec131a6262faf77d1
Shahin <meshahin007@gmail.com>
2018-11-18 00:04:52 +0600

    commit

---
+++ logical-operator.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+<?php  
+	
+
+	// $x = 10;
+	// $y = 20;
+
+	// if($x == $y || 1 == 1){
+	// 	echo "true";
+	// } else{
+	// 	echo "false";
+	// }
+
+
+	$x = 10;
+	$y = 10;
+
+	if($x == $y xor 2 == 1){
+		echo "true";
+	} else{
+		echo "false";
+	}
+
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
