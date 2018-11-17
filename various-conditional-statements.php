3a944637d8216d47f54414075a52d961ce4c6136
Shahin <meshahin007@gmail.com>
2018-11-18 00:22:50 +0600

    commit

---
+++ various-conditional-statements.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+<?php  
+	
+
+	$x = 2;
+
+	if($x == 1){
+		echo 'True';
+	}
+	elseif($x == 2){
+		echo 'Also True';
+	}
+	elseif($x == 3){
+		echo 'Also True';
+	}
+	elseif($x == 4){
+		echo 'Also True';
+	}
+	elseif($x == 5){
+		echo 'Also True';
+	}
+	elseif($x == 6){
+		echo 'Also True';
+	}
+	else{
+		echo 'False';
+	}
+
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
