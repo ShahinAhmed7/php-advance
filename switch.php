985efb5b019a70fabd410b2423e196528f22afc4
Shahin <meshahin007@gmail.com>
2018-11-19 01:16:04 +0600

    switch update

---
+++ switch.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+<div class="style">
+	<h2>PHP TUTORIAL</h2>
+	
+<div class="body_text">
+
+
+
+<?php  
+	
+
+	$x = 3;
+
+	switch($x){
+		case 'name';
+			echo 'Shahin';
+		break;
+
+		case 4:
+			echo 'Hi shahin';
+		break;
+		default:
+			echo 'Default';
+	}
+
+?>
+</div>
+
+
+</div>
+
+</body>
+</html>
